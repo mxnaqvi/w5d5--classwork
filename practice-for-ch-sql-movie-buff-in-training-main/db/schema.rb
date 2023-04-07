a2565e8e2b2937f189b1cea1ecb7a4c3ffc0344b
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-04-07 11:59:34 -0400

    starting again

--- practice-for-ch-sql-movie-buff-in-training-main/db/schema.rb
+++ practice-for-ch-sql-movie-buff-in-training-main/db/schema.rb
@@ -10,33 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_12_154822) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_16_020625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "courses", force: :cascade do |t|
-    t.string "name", null: false
-    t.bigint "prereq_id"
-    t.bigint "instructor_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["instructor_id"], name: "index_courses_on_instructor_id"
-    t.index ["prereq_id"], name: "index_courses_on_prereq_id"
-  end
-
-  create_table "enrollments", force: :cascade do |t|
-    t.bigint "course_id", null: false
-    t.bigint "student_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["course_id"], name: "index_enrollments_on_course_id"
-    t.index ["student_id"], name: "index_enrollments_on_student_id"
-  end
-
-  create_table "users", force: :cascade do |t|
+  create_table "actors", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_actors_on_name"
   end
 
+  create_table "castings", force: :cascade do |t|
+    t.bigint "actor_id", null: false
+    t.bigint "movie_id", null: false
+    t.integer "ord", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["actor_id", "movie_id"], name: "index_castings_on_actor_id_and_movie_id", unique: true
+    t.index ["movie_id"], name: "index_castings_on_movie_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "yr", null: false
+    t.float "score", null: false
+    t.integer "votes", null: false
+    t.bigint "director_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["director_id"], name: "index_movies_on_director_id"
+    t.index ["title"], name: "index_movies_on_title"
+  end
+
+  add_foreign_key "castings", "actors"
+  add_foreign_key "castings", "movies"
+  add_foreign_key "movies", "actors", column: "director_id"
 end
